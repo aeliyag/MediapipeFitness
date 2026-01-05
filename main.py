813bb3447c7d881d4467786f69edf62cabbf63bd
aeliyag <aeliyagrover88@gmail.com>
2026-01-05 13:46:01 -0600

    stuff

---
+++ main.py
@@ -0,0 +1,44 @@
+import mediapipe as mp 
+import cv2 
+
+# Using the " Creating Your Own AI Fitness Trainer: Analyzing Squats with MediaPipe"
+# Youtube Tutorial: https://www.youtube.com/watch?v=Ae3SPjsXETc
+# open virtual environment
+# install opencv and mediapipe 
+# pip install opencv-python
+# pip install mediapipe
+#  
+
+pose = mp.solutions.pose.Pose(
+    static_image_mode           = True, 
+    model_complexity          = 1,
+    smooth_landmarks         = True,
+    min_detection_confidence = 0.5,
+    min_tracking_confidence  = 0.5
+)
+
+drawing = mp.solutions.drawing_utils 
+
+imgRGB = cv2.imread("human-pose.png", 1) # image to read in 
+
+result = pose.process(imgRBG) # process the image
+
+if result.pose_landmarks: 
+    drawing.draw_landmarks(
+        image = img, 
+        landmark_list = result.pose_landmarks,
+        connections = mp.solutions.pose.POSE_CONNECTIONS, 
+        landmark_drawing_spec = drawing.DrawingSpec (
+            color = (255, 255, 255), 
+            thinkness = 7, 
+            circle_radius = 4
+        ), 
+        connection_drawing_spec = drawing.DrawingSpec (
+            color = (0, 0, 255), 
+            thinkness = 11, 
+            circle_radius = 3
+        )
+    )
+
+print(result.pose_landmarks) 
+cv.imwrite("Pose.js", img)
